bb747afcf96121e7f20a6ff404f5ada9c002780f
Carlos Hernadnez Perez <carloshe@qrsof.com>
2021-08-25 12:56:24 -0500

    v2 final

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 Openpay
 
+
+![img.png](img.png)
+
 ## Installation
 
 ```sh
@@ -10,6 +13,8 @@ npm install openpay-react-native
 
 ## Usage
 
+You can use our default form or you can use a custom form
+
 ```js
 import OpenpayReactNative from "openpay-react-native";
 
@@ -17,8 +22,8 @@ import OpenpayReactNative from "openpay-react-native";
 
 <Openpay
   isProductionMode={false}
-  merchantId="m2tmftuv5jao96rrezj2"
-  publicKey="pk_d5e9bff37db4468da3f80148bb94f263"
+  merchantId="merchantId"
+  publicKey="publicKey"
   //address={address} //optional
   successToken={successToken}
   failToken={failToken}
@@ -28,7 +33,141 @@ import OpenpayReactNative from "openpay-react-native";
   custom={false}
 />
 
-See the example project to findout how to use the library
+*Yo can see the example project
+```
+
+###Custom labes and place holders
+
+```ts
+<Openpay
+                        isProductionMode={false}
+                        merchantId="m2tmftuv5jao96rrezj2"
+                        publicKey="pk_d5e9bff37db4468da3f80148bb94f263"
+                        //address={address} //optional
+                        successToken={successToken}
+                        failToken={failToken}
+                        loading={loading}
+                        deviceSession={deviceSession}
+                        buttonText="Pagar"
+                        custom={false}
+                        labels={
+                            {
+                                holder: 'Nombre completo',
+                                expiration: 'FECHA'
+                            }
+                        }
+                        placeholders={{
+                            holder: "Nombre Apellidos",
+                            number: 'xxxx xxxx **** ****',
+                            expiration: 'MM/YY',
+                            cvv: 'CVV'
+                        }}
+                    />
+```
+
+### Device session id
+With the inicilization of de component you have to pass a callback that will be called with the deviceSession
+
+```js
+const deviceSession = (response) => {
+    console.log('deviceSession');
+    console.log(response);
+  };
+```
+
+### Custom form
+With the inicialization of the component you have to pass custom to true: custom={true}. With this, the form just call the device
+session callback and let you tokenize the card manually with the exported function:
+
+```ts
+import {Openpay,createTokenWithCard} from 'openpay-react-native';
+```
+
+```ts
+createTokenWithCard(
+  {
+    holder_name: 'Nombre Prueba',
+    cvv2: '111',
+    expiration_month: '12',
+    card_number: '424242424242424242',
+    expiration_year: '25',
+    isProductionMode: false,
+    merchantId: 'merchantId',
+    publicKey: 'publicKey',
+  }
+).then((response) => {
+  console.log(response);
+});
+```
+
+
+##Full example
+```ts
+import * as React from 'react';
+import { useEffect } from 'react';
+import {
+  Alert,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  View,
+} from 'react-native';
+import Openpay from 'openpay-react-native';
+
+export default function App() {
+  const successToken = (response) => {
+    console.log(response);
+    Alert.alert('Tokne generado', response.id, [
+      { text: 'OK', onPress: () => console.log('OK Pressed') },
+    ]);
+  };
+  const failToken = (response) => {
+    console.log(failToken);
+    console.log(response);
+    Alert.alert('Datos inválidos', [
+      { text: 'OK', onPress: () => console.log('OK Pressed') },
+    ]);
+  };
+
+  const deviceSession = (response) => {
+    console.log('deviceSession');
+    console.log(response);
+  };
+  
+  return (
+    <SafeAreaView>
+      <StatusBar barStyle={'light-content'} />
+      <ScrollView
+        style={styles.scrollView}
+        contentInsetAdjustmentBehavior="automatic">
+        <View style={styles.sectionContainer}>
+          <Openpay
+            isProductionMode={false}
+            merchantId="merchantId"
+            publicKey="publicKey"
+            //address={address} //optional
+            successToken={successToken}
+            failToken={failToken}
+            deviceSession={deviceSession}
+            buttonText="Pagar"
+            custom={false}
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  scrollView: {
+  },
+});
+
 ```
 
 ## Contributing
--- src/index.js
+++ src/index.js
@@ -17,295 +17,313 @@ let sessionId = uuidv4();
 sessionId = sessionId.toUpperCase().replace(/-/g, '');
 
 export const createTokenWithCard = async (data) => {
-  const method = 'POST';
-  const resource = 'tokens';
-  return await sendFunction(method, resource, data);
+    const method = 'POST';
+    const resource = 'tokens';
+    return await sendFunction(method, resource, data);
 };
 
 const sendFunction = (method, resource, data) => {
-  const username = data.publicKey;
-  const url = vsprintf('%s/v1/%s/%s', [
-    data.isProductionMode ? API_URL_PRODUCTION : API_URL_SANDBOX,
-    data.merchantId,
-    resource,
-  ]);
-  const authorization =
-    'Basic ' + new Buffer(username + ':').toString('base64');
+    const username = data.publicKey;
+    const url = vsprintf('%s/v1/%s/%s', [
+        data.isProductionMode ? API_URL_PRODUCTION : API_URL_SANDBOX,
+        data.merchantId,
+        resource,
+    ]);
+    const authorization =
+        'Basic ' + new Buffer(username + ':').toString('base64');
 
-  return fetch(url, {
-    method: method,
-    mode: 'no-cors',
-    cache: 'no-cache',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': authorization,
-    },
-    body: JSON.stringify(data),
-  })
-    .then((response) => response.json()) // parses response to JSON
-    .catch((error) => {
-      throw error;
-    });
+    return fetch(url, {
+        method: method,
+        mode: 'no-cors',
+        cache: 'no-cache',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': authorization,
+        },
+        body: JSON.stringify(data),
+    })
+        .then((response) => response.json()) // parses response to JSON
+        .catch((error) => {
+            throw error;
+        });
 };
 
 export default class Openpay extends Component {
-  creditCardRef;
+    creditCardRef;
 
-  constructor(props) {
-    super(props);
-
-    this.creditCardRef = React.createRef();
-
-    if (!this.validateProps(props)) {
-      throw new Error('Openpay component requires all the specified props.');
-    }
-    this.state = {
-      sessionId: undefined,
-      uri: '',
-      injectedJavaScript: '',
-      loading: false,
-    };
-  }
-
-  componentDidMount() {
-    this.createDeviceSessionId();
-  }
-
-  componentWillReceiveProps(nextProps) {
-    this.setState(() => ({loading: nextProps.loading}));
-  }
-
-  validateProps = (props) => {
-    // Se valida que existan las propiedad requeridas
-    if (
-      !props.hasOwnProperty('isProductionMode') ||
-      !props.hasOwnProperty('merchantId') ||
-      !props.hasOwnProperty('publicKey') ||
-      !props.hasOwnProperty('successToken') ||
-      !props.hasOwnProperty('failToken') ||
-      !props.hasOwnProperty('custom')
-    ) {
-      return false;
-    }
-
-    // Se validan los tipos de dato
-    if (!isBoolean(props.isProductionMode)) {
-      return false;
-    }
-
-    if (!isString(props.merchantId)) {
-      return false;
-    }
-
-    if (!isString(props.publicKey)) {
-      return false;
-    }
-
-    if (!isFunction(props.successToken)) {
-      return false;
-    }
-
-    if (!isFunction(props.failToken)) {
-      return false;
-    }
-
-    if (!isBoolean(props.custom)) {
-      return false;
-    }
-
-    if (!isBoolean(props.custom)) {
-      return false;
-    } else if (props.custom && !isFunction(props.deviceSession)) {
-      return false;
-    }
-
-    if (props.hasOwnProperty('address') && !isObject(props.address)) {
-      return false;
-    }
-
-    return true;
-  };
-
-  createDeviceSessionId = () => {
-    let identifierForVendor = this.identifierForVendor();
-    identifierForVendor = identifierForVendor.replace(/-/g, '');
-
-    const uri = vsprintf('%s/oa/logo.htm?m=%s&s=%s', [
-      this.props.isProductionMode ? API_URL_PRODUCTION : API_URL_SANDBOX,
-      this.props.merchantId,
-      sessionId,
-    ]);
-    const injectedJavaScript = vsprintf('var identifierForVendor = "%s";', [
-      identifierForVendor,
-    ]);
-
-    this.setState(() => ({uri, injectedJavaScript}));
-    this.props.deviceSession(sessionId);
-  };
-
-  identifierForVendor = () => {
-    let deviceSerial = '';
-    try {
-      if (window.Expo && window.Expo.Constants.appOwnership === 'expo') {
-        console.log('Running in expo');
-        deviceSerial =
-          typeof Expo.Constants.installationId !== 'undefined'
-            ? Expo.Constants.installationId
-            : Expo.Constants.deviceId;
-      } else {
-        deviceSerial = DeviceInfo.getUniqueId();
-      }
-    } catch (e) {
-      console.log('error reading device ID', e);
-    }
-
-    return deviceSerial;
-  };
-
-  tokenize = async (form) => {
-    console.log('******** tokenize ******');
-    const cardForm = form;
-
-    this.setState(() => ({loading: true}));
-
-    const card = cardForm;
-    const expirationDate = card.expiration.split('/');
-    const requestData = {
-      card_number: card.number.replace(/ /g, ''),
-      holder_name: card.holder,
-      cvv2: card.cvv,
-      expiration_month: expirationDate[0],
-      expiration_year: expirationDate[1],
-      isProductionMode: this.props.isProductionMode,
-      merchantId: this.props.merchantId,
-      publicKey: this.props.publicKey,
+    labels = {
+        holder: 'Full Name',
+        expiration: 'Expiration Date',
+        cvv: 'Security Code',
     };
 
-    if (this.props.address) {
-      requestData.address = this.props.address;
+    placeholders = {
+        holder: "Holder's Name",
+        number: '**** **** **** ****',
+        expiration: 'MM/YY',
+        cvv: 'CVV',
+    };
+
+    constructor(props) {
+        super(props);
+
+        this.creditCardRef = React.createRef();
+
+        if (!this.validateProps(props)) {
+            throw new Error('Openpay component requires all the specified props.');
+        }
+        this.state = {
+            sessionId: undefined,
+            uri: '',
+            injectedJavaScript: '',
+            loading: false,
+        };
     }
 
-    try {
-      const response = await createTokenWithCard(requestData);
-      if (response.error_code) {
-        this.props.failToken(response);
-      } else {
-        this.props.successToken(response);
-      }
-    } catch (error) {
-      this.props.failToken(error);
+    componentDidMount() {
+        this.createDeviceSessionId();
     }
-  };
 
-  render() {
-    const labels = {
-      holder: 'Full Name',
-      expiration: 'Expiration Date',
-      cvv: 'Security Code',
+    validateProps = (props) => {
+        // Se valida que existan las propiedad requeridas
+        if (
+            !props.hasOwnProperty('isProductionMode') ||
+            !props.hasOwnProperty('merchantId') ||
+            !props.hasOwnProperty('publicKey') ||
+            !props.hasOwnProperty('successToken') ||
+            !props.hasOwnProperty('failToken') ||
+            !props.hasOwnProperty('custom')
+        ) {
+            return false;
+        }
+
+        // Se validan los tipos de dato
+        if (!isBoolean(props.isProductionMode)) {
+            return false;
+        }
+
+        if (!isString(props.merchantId)) {
+            return false;
+        }
+
+        if (!isString(props.publicKey)) {
+            return false;
+        }
+
+        if (!isFunction(props.successToken)) {
+            return false;
+        }
+
+        if (!isFunction(props.failToken)) {
+            return false;
+        }
+
+        if (!isBoolean(props.custom)) {
+            return false;
+        }
+
+        if (!isBoolean(props.custom)) {
+            return false;
+        } else if (props.custom && !isFunction(props.deviceSession)) {
+            return false;
+        }
+
+        if (props.hasOwnProperty('address') && !isObject(props.address)) {
+            return false;
+        }
+
+        if (props.hasOwnProperty('labels')) {
+            if (!isObject(props.labels)) {
+                return false;
+            } else {
+                this.labels.cvv = props.labels.cvv || this.labels.cvv;
+                this.labels.holder = props.labels.holder || this.labels.holder;
+                this.labels.expiration = props.labels.expiration || this.labels.expiration;
+            }
+        }
+
+        if (props.hasOwnProperty('placeholders')) {
+            if (!isObject(props.labels)) {
+                return false;
+            } else {
+                this.placeholders.holder = props.placeholders.holder || this.placeholders.holder;
+                this.placeholders.number = props.placeholders.number || this.placeholders.number;
+                this.placeholders.expiration = props.placeholders.expiration || this.placeholders.expiration;
+                this.placeholders.cvv = props.placeholders.cvv || this.placeholders.cvv;
+            }
+        }
+
+        return true;
     };
 
-    const placeholders = {
-      holder: "Holder's Name",
-      number: '**** **** **** ****',
-      expiration: 'MM/YY',
-      cvv: 'CVV',
+    createDeviceSessionId = () => {
+        let identifierForVendor = this.identifierForVendor();
+        identifierForVendor = identifierForVendor.replace(/-/g, '');
+
+        const uri = vsprintf('%s/oa/logo.htm?m=%s&s=%s', [
+            this.props.isProductionMode ? API_URL_PRODUCTION : API_URL_SANDBOX,
+            this.props.merchantId,
+            sessionId,
+        ]);
+        const injectedJavaScript = vsprintf('var identifierForVendor = "%s";', [
+            identifierForVendor,
+        ]);
+
+        this.setState(() => ({uri, injectedJavaScript}));
+        this.props.deviceSession(sessionId);
     };
 
-    const {uri, injectedJavaScript, loading} = this.state;
+    identifierForVendor = () => {
+        let deviceSerial = '';
+        try {
+            if (window.Expo && window.Expo.Constants.appOwnership === 'expo') {
+                console.log('Running in expo');
+                deviceSerial =
+                    typeof Expo.Constants.installationId !== 'undefined'
+                        ? Expo.Constants.installationId
+                        : Expo.Constants.deviceId;
+            } else {
+                deviceSerial = DeviceInfo.getUniqueId();
+            }
+        } catch (e) {
+            console.log('error reading device ID', e);
+        }
 
-    const handleSubmit = async () => {
-      if (this.creditCardRef.current) {
-        const {error, data} = this.creditCardRef.current.submit();
-        console.log('ERROR: ', error);
-        console.log('CARD DATA: ', data);
-        await this.tokenize(data);
-      }
+        return deviceSerial;
     };
 
-    const background = '#ff7600';
-    const placeholderTextColor = '#ffffff';
+    tokenize = async (form) => {
+        console.log('******** tokenize ******');
+        const cardForm = form;
 
-    const CardWrapper = () => {
-      if (!this.props.custom) {
+        this.setState(() => ({loading: true}));
+
+        const card = cardForm;
+        const expirationDate = card.expiration.split('/');
+        const requestData = {
+            card_number: card.number.replace(/ /g, ''),
+            holder_name: card.holder,
+            cvv2: card.cvv,
+            expiration_month: expirationDate[0],
+            expiration_year: expirationDate[1],
+            isProductionMode: this.props.isProductionMode,
+            merchantId: this.props.merchantId,
+            publicKey: this.props.publicKey,
+        };
+
+        if (this.props.address) {
+            requestData.address = this.props.address;
+        }
+
+        try {
+            const response = await createTokenWithCard(requestData);
+            if (response.error_code) {
+                this.props.failToken(response);
+            } else {
+                this.props.successToken(response);
+            }
+        } catch (error) {
+            this.props.failToken(error);
+        }
+    };
+
+    render() {
+
+        const {uri, injectedJavaScript, loading} = this.state;
+
+        const handleSubmit = async () => {
+            if (this.creditCardRef.current) {
+                const {error, data} = this.creditCardRef.current.submit();
+                console.log('ERROR: ', error);
+                console.log('CARD DATA: ', data);
+                await this.tokenize(data);
+            }
+        };
+
+        const background = '#ff7600';
+        const placeholderTextColor = '#ffffff';
+
+        const CardWrapper = () => {
+            if (!this.props.custom) {
+                return (
+                    <KeyboardAvoidingView
+                        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+                        keyboardVerticalOffset={20}
+                    >
+                        <CreditCard
+                            ref={this.creditCardRef}
+                            labels={this.labels}
+                            placeholders={this.placeholders}
+                            background={background}
+                            placeholderTextColor={placeholderTextColor}
+                        />
+                        <Button
+                            buttonStyle={styles.button}
+                            title={this.props.buttonText}
+                            onPress={handleSubmit}
+                        />
+                    </KeyboardAvoidingView>
+                );
+            } else {
+                return <View/>;
+            }
+        };
+
         return (
-          <KeyboardAvoidingView
-            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-            keyboardVerticalOffset={20}
-          >
-            <CreditCard
-              ref={this.creditCardRef}
-              labels={labels}
-              placeholders={placeholders}
-              background={background}
-              placeholderTextColor={placeholderTextColor}
-            />
-            <Button
-              buttonStyle={styles.button}
-              title={this.props.buttonText}
-              onPress={handleSubmit}
-            />
-          </KeyboardAvoidingView>
+            <View style={styles.container}>
+                <CardWrapper/>
+                <WebView
+                    source={{uri: uri}}
+                    injectedJavaScript={injectedJavaScript}
+                />
+            </View>
         );
-      } else {
-        return <View/>;
-      }
-    };
-
-    return (
-      <View style={styles.container}>
-        <CardWrapper/>
-        <WebView
-          source={{uri: uri}}
-          injectedJavaScript={injectedJavaScript}
-        />
-      </View>
-    );
-  }
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    paddingBottom: 400,
-  },
-  button: {
-    height: 45,
-    backgroundColor: '#ff7600',
-    borderColor: '#ff7600',
-    borderWidth: 1,
-    borderRadius: 8,
-    marginBottom: 5,
-    justifyContent: 'center',
-    marginTop: 30,
-    padding: 5,
-    width: '100%',
-  },
+    container: {
+        paddingBottom: 400,
+    },
+    button: {
+        height: 45,
+        backgroundColor: '#ff7600',
+        borderColor: '#ff7600',
+        borderWidth: 1,
+        borderRadius: 8,
+        marginBottom: 5,
+        justifyContent: 'center',
+        marginTop: 30,
+        padding: 5,
+        width: '100%',
+    },
 });
 
 const isFunction = (value) => {
-  return typeof value === 'function';
+    return typeof value === 'function';
 };
 
 const isObject = (value) => {
-  return value && typeof value === 'object' && value.constructor === Object;
+    return value && typeof value === 'object' && value.constructor === Object;
 };
 
 const isString = (value) => {
-  return typeof value === 'string' || value instanceof String;
+    return typeof value === 'string' || value instanceof String;
 };
 
 const isBoolean = (value) => {
-  return typeof value === 'boolean';
+    return typeof value === 'boolean';
 };
 
 const isNumber = (value) => {
-  return typeof value === 'number' && isFinite(value);
+    return typeof value === 'number' && isFinite(value);
 };
 
 const isNull = (value) => {
-  return value === null;
+    return value === null;
 };
 
 // Returns if a value is undefined
 const isUndefined = (value) => {
-  return typeof value === 'undefined';
+    return typeof value === 'undefined';
 };
